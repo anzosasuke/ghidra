8776e1997c9fe72d803d3e4b5d0c7c0fbf1d604f
anzosasuke <33804183+anzosasuke@users.noreply.github.com>
2022-01-07 11:42:25 -0600

    Update ChkDominanceAlgorithm.java

--- Ghidra/Framework/Graph/src/main/java/ghidra/graph/algo/ChkDominanceAlgorithm.java
+++ Ghidra/Framework/Graph/src/main/java/ghidra/graph/algo/ChkDominanceAlgorithm.java
@@ -232,7 +232,7 @@ public class ChkDominanceAlgorithm<V, E extends GEdge<V>> extends AbstractDomina
 			}
 
 			V dominator = getImmediateDominator(v);
-			if (!Objects.equals(dominator, v)) {
+			if (!Objects.equals(dominator, v) && dominator != null) {
 				dg.addEdge(new DefaultGEdge<>(dominator, v));
 			}
 		}
